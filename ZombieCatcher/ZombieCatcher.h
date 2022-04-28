af7f7605e4042ef4ea8adbbae425a8cec8b66ce9
TBD <weinanshao@163.com>
2022-04-28 10:40:01 +0800

    Initial Commit

---
+++ ZombieCatcher/ZombieCatcher.h
@@ -0,0 +1,18 @@
+//
+//  ZombieCatcher.h
+//  ZombieCatcher
+//
+//  Created by TBD on 2022/4/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZombieCatcher.
+FOUNDATION_EXPORT double ZombieCatcherVersionNumber;
+
+//! Project version string for ZombieCatcher.
+FOUNDATION_EXPORT const unsigned char ZombieCatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZombieCatcher/PublicHeader.h>
+
+
